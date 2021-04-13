feca440c7b87274e0f09e5d263b646d121f57b87
Katarina Orlovska <k.orlovska@posteo.de>
2021-04-13 15:03:00 +0200

    Cintia and Katka

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length ++
+  }
 
-  get(pos) {}
+  get(pos) {
+     if (pos<this.items.length) return this.items[pos] 
+     else  {throw new Error('OutOfBounds')} ;
 
-  max() {}
+     // pos<this.items.length ? 
+  }
 
-  min() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items)
+    } else throw new Error('EmptySortedList');
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items)
+    } else throw new Error('EmptySortedList');
+  }
 
-  avg() {}
+  sum() {
+    return this.items.length > 0 ? this.items.reduce((a,b) => a+b) : 0;
+  }
+
+  avg() {
+    let avg = this.sum()/this.items.length;
+    if(this.items.length > 0) {return avg }
+    else  { throw new Error('EmptySortedList')};
+  }
 }
 
 module.exports = SortedList;
+
+// let list = new SortedList();
+// list.add(1);
+// console.log(list.items.length);
+
+// let arr = [1,5,6,3]
+// arr.push(2)
+// arr.sort( (a,b) => a-b ) 
+// console.log(arr)
